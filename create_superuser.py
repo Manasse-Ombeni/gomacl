88af8ba14ff41e9056b36a8aa59cb5ad85ddbd6e
Manasse-Ombeni <ombenation16@gmail.com>
2026-03-04 18:55:34 +0300

    relance superuser

--- create_superuser.py
+++ create_superuser.py
@@ -8,8 +8,8 @@ from django.contrib.auth.models import User
 
 # Créer le superuser en production
 username = 'ombeni'
-password = 'GomaCL2026@Secure!'  # Mot de passe sécurisé pour la production
-email = 'admin@gomacl.com'
+password = 'Goma-2026'  # Mot de passe sécurisé pour la production
+email = 'ombenation16@gmail.com'
 
 if not User.objects.filter(username=username).exists():
     User.objects.create_superuser(username=username, email=email, password=password)
