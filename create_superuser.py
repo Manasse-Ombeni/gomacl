f75f4ae726682b61d2864808834c4e3c3da5db6b
Manasse-Ombeni <ombenation16@gmail.com>
2026-03-04 19:31:27 +0300

    admin ombeni auto create

--- create_superuser.py
+++ create_superuser.py
@@ -6,13 +6,12 @@ django.setup()
 
 from django.contrib.auth.models import User
 
-# Créer le superuser en production
 username = 'ombeni'
-password = 'Goma-2026'  # Mot de passe sécurisé pour la production
 email = 'ombenation16@gmail.com'
+password = 'Goma-2026'
 
 if not User.objects.filter(username=username).exists():
     User.objects.create_superuser(username=username, email=email, password=password)
-    print(f"✅ Superuser '{username}' créé avec succès !")
+    print(f"Superuser '{username}' créé avec succès !")
 else:
-    print(f"⚠️ L'utilisateur '{username}' existe déjà.")
+    print(f"L'utilisateur '{username}' existe déjà.")
